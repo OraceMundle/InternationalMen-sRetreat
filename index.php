2f82c8ae4ba176b53b1e3bbcf2dbd550533bbbfc
OraceMundle <71685178+OraceMundle@users.noreply.github.com>
2020-12-01 18:15:05 -0500

    Add Video/Pics

--- index.php
+++ index.php
@@ -3,8 +3,27 @@
 $title = "International Men's Retreat";
 require_once 'includes/header.php'; 
 
-
 ?>
 
 
  <h1><?php echo $title ?></h1> 
+
+
+ <object width="1000" height="500" data="https://www.youtube.com/v/a9gkTvkZ8IM" type="application/x-shockwave-flash">
+ <param name="src" value="https://www.youtube.com/v/a9gkTvkZ8IM" /></object>
+
+
+
+
+
+
+ <?php 
+
+
+require_once 'includes/footer.php'; 
+
+?>
+
+
+
+
