1593a909164c276367f3a34367de5befa48af87e
OraceMundle <71685178+OraceMundle@users.noreply.github.com>
2020-12-09 12:56:21 -0500

    updates

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit5c06493c1644fd0f26d73d62ff5a5d35
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit5c06493c1644fd0f26d73d62ff5a5d35::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire5c06493c1644fd0f26d73d62ff5a5d35($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire5c06493c1644fd0f26d73d62ff5a5d35($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
