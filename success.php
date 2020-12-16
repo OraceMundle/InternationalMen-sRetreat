b5f5977dd3523d438d77db3494204a1be37bb362
OraceMundle <71685178+OraceMundle@users.noreply.github.com>
2020-12-16 13:44:55 -0500

    Update success.php

--- success.php
+++ success.php
@@ -33,8 +33,7 @@
 
 <!-- Bootstrap Card Tiles component -->
 
-<img src=" <?php echo  $_POST['avatar']; /*empty($destination) ? "uploads/defaultimage.png" : $destination; */ ?>" class="rounded-circle" style="width: 20%, height: 20%;"/>
-<!--img src=" <?php //echo $destination ?>" class="rounded-circle" style="width: 20%, height: 20%;" />--> 
+<img src=" <?php echo  $_POST['avatar']; ?>" class="rounded-circle" style="width: 20%, height: 20%;"/>
 <br />
 <br />
 <div class="card" style="width: 25rem;">
@@ -49,33 +48,11 @@
 
 <!--end of Bootstrap Card Tiles component -->
 
-<!--This Prints out information passed to the action page using the GET method -->
-<!-- Bootstrap Card Tiles component 
-<div class="card" style="width: 25rem;">
-    <div class="card-body">
-        <h5 class="card-title"><?php //echo $_GET['firstname'] . ' ' . $_GET['lastname'];   ?></h5>
-        <h6 class="card-subtitle mb-2 text-muted"><?php  //echo $_GET['speciality'];  ?></h6>
-        <p class="card-text">Date of Birth: <?php    //echo $_GET['dob']; ?> </p>
-        <p class="card-text">Email Address: <?php    // echo $_GET['exampleInputEmail1']; ?> </p>
-        <p class="card-text">Phone Number: <?php    //echo $_GET['phone']; ?> </p>
-
-         Links not being used
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-        
-
-    </div>
-</div>
- end of Bootstrap Card Tiles component -->
-
 
 <br />
 <br />
 <a href="index.php" class="btn btn-outline-info">Home</a>
-
-<!--
-<a href="viewrecords.php" class="btn btn-info">Back to List</a> 
--->
+<a href="regform1.php" class="btn btn-outline-info">Register Here</a> 
 
 
 <br />
