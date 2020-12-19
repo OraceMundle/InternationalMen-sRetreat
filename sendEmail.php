ec5d5af8220dd66d6895081c127dd8adb40f975d
OraceMundle <71685178+OraceMundle@users.noreply.github.com>
2020-12-18 20:42:10 -0500

    email updates

--- sendEmail.php
+++ sendEmail.php
@@ -1,7 +1,7 @@
 <?php
 
 
-use Dotenv\Dotenv;
+//use Dotenv\Dotenv;
 
 require 'vendor\autoload.php';
 require  'vendor\vlucas\phpdotenv\src\Dotenv.php';
@@ -13,23 +13,23 @@ require  'vendor\vlucas\phpdotenv\src\Dotenv.php';
         public static function SendMail($to, $subject, $content){
 
                     //variable key stores API
-                    //$key=Dotenv\Dotenv::createImmutable(__DIR__,"/..");
-                    //$key->load();
-                    
-                    //const MY_KEY = process.env.INTERNATIONAL_MENS_APP_API_KEY;
-
-                    //$key = getenv('API_KEY');
+                    //use Dotenv\Dotenv;   
+                                    
+                    //Dotenv::load(__DIR__);
+                    //$key=getenv('INTERNATIONAL_MENS_APP_API_KEY');
+                                                            
                                        
                     //email object calling a new object of SendGrid
                     $email = new \SendGrid\Mail\Mail();
-                    $email->setFrom("mundlepoo@hotmail.com","Orace Mundle" );
+                    $email->setFrom("noreply@internationalmensretreat.com","Orace Mundle" );
                     $email->setSubject($subject);
                     $email->addTo($to);
                     $email->addContent("text/plain", $content);
                     // $email->addContent("text/html", $content);
 
                     //instantiating sendgrid object
-                    $sendgrid = new \SendGrid($key);
+                    $sendgrid = new \SendGrid(getenv('INTERNATIONAL_MENS_APP_API_KEY'));
+                    //$sendgrid = new \SendGrid($key);
 
                     try {
                         //call function send stored in respomse
